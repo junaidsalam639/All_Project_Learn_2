59bc2d19344b80e149a595ebcb8d20735840a5ab
Muhammad Ahsan <ahsan663399@gmail.com>
2025-02-09 14:23:42 +0500

    'all_project'

---
+++ hackaton-SMIT-Attendence-App/src/app/page.js
@@ -0,0 +1,10 @@
+import Login from "./Component/Login";
+
+export default function Home() {
+  return <>
+   <Login />
+  </>;
+}
+
+
+// https://www.figma.com/file/ZyGunJ8WtYjvoKOTQTDlVZ/Attendance-App?node-id=515%3A3&mode=dev
